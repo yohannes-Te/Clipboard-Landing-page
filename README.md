78162fb3ec1288b1b9ac4fc04482b473368f3d3d
yohannes <yohannes22t@gmail.com>
2024-03-19 21:44:34 +0300

    final commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Frontend Mentor - Clipboard landing page solution
+
+### Screenshot
+
+![](./design/desktop-preview.jpg)
+
+### Links
+
+- Solution URL: (https://github.com/yohannes-Te/Clipboard-Landing-page)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- Mobile-first workflow
+- [tailwindcss](https://tailwindcss.com/) - CSS framework
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+
+    extend: {
+      colors: {
+        StrongCyan: "hsl(171, 66%, 44%)",
+        LightBlue: "hsl(233, 100%, 69%)",
+        DarkGrayishBlue: "hsl(210, 10%, 33%)",
+        GrayishBlue: "hsl(201, 11%, 66%)",
+      },
+      fontFamily:{
+        sans:['Bai Jamjuree', 'san-serif']
+      },
+    },
+  },
+  plugins: [],
+};
